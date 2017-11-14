0fb9dec772bd918e7afc9807a8e252befaa4c2d8
Juan Moris <moris_juan@hotmail.com>
2017-11-14 17:57:28 -0300

    Sliding Elements Up and Down

--- js/script.js
+++ js/script.js
@@ -1,15 +1,12 @@
 $(function() {
   // jQuery goes here...
 
-  // Uncomment this line to fade out the red box on page load
+  // $(".blue-box").slideUp(2000);//This will make the element disappear and will set the display to none.
+  // $(".blue-box").slideDown(2000);
 
-  //Showing and Hiding Elements.
-  //$(".blue-box").hide(); //It's going to set the display value to none.
-  //$(".blue-box").show();
+  $(".blue-box").slideToggle(2000);//This is going to slide up the element if it's currently shown and it's gonna slide down 
+  // the element if it's currently hidde.
 
-  //$(".blue-box").toggle();// this will show all elements that are hidden and it will hide all elements that are already shown.
-
-  $(".blue-box").hide(1000); //This going to be another animation. With this we can see an naimation that is fade out and shrink
-  //to the upper left corner
-  $(".blue-box").show(1000);
+  $("p").hide();
+  $("p").slideDown(1000);
 });
