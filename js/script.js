727e87fc55cbe35729cd36f334869bd6d35c0b59
Juan Moris <moris_juan@hotmail.com>
2017-11-14 17:08:36 -0300

    A Pitfall With fadeIn/fadeOut vs. fadeTo

--- js/script.js
+++ js/script.js
@@ -2,21 +2,20 @@ $(function() {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000); // 2000 milliseconds or 2 seconds
-  // $(".red-box").fadeOut(); // by default is 400 milliseconds
-  // $(".red-box").fadeOut("slow"); // 600 milliseconds
-  // $(".red-box").fadeOut("fast"); // 200 milliseconds
-  // $(".green-box").fadeOut(2000);
 
-  //Fade In
-  // $(".red-box").fadeIn(2000);
+  /*A Pitfall With fadeIn/fadeOut vs. fadeTo
+  $(".green-box").fadeOut(1000); // --> with this once it fade out it's actually removed form the layout. In CCS terms this green-box class
+  has a style of display: none applied, which is the reason why all the other elements on the page are realign as if that element wasn't 
+  there at all. */
 
-  //Fade To
-  $(".red-box").fadeTo(3000, 0.2); // fisrt argument is the duration and the second is the opacity you want to fade to.
-  $(".green-box").fadeTo(2000, 0.5);
-  $(".blue-box").fadeTo(1000, 0.8);
+  /*In some cases you may want that but in some other cases you may also just want to fade out without being removed from the layout, so we 
+  don't want the display to be set to none. You just want the element to be invisible.*/
 
-  //Fade Toggle
-  $(".red-box").fadeToggle(); //jQuery also takes care of fading the element back into the opacity that it had before. In this case it fade out
-  $(".red-box").fadeToggle(); // and then fade in to the opacity that it had before that is 20%
+  /*And this is when the fadeTo function comes in
+  $(".green-box").fadeTo(1000, 0); //here is going to fade to opacity 0, so now this has no display none anymore, it's only set the opacity to 0.
+  so the element becomes invisible but it's not going to change the arrangement of the other element.*/
+
+  //Example
+  $(".green-box").fadeOut(1000);
+  $(".green-box").fadeTo(1000, 0.5);
 });
