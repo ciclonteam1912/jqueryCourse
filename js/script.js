05107a8f4b85b35a3ea99786e4bed02655c54490
Juan Moris <moris_juan@hotmail.com>
2017-11-14 20:10:17 -0300

    Custom Animations

--- js/script.js
+++ js/script.js
@@ -2,8 +2,21 @@ $(function() {
   // jQuery goes here...
 
   //Moving elements
+  /* $(".blue-box").animate({
+   	"opacity": "0"	
+  }, 1000); //This would be the same to fadeTo(1000, 0)*/
+
+  //But with custom animations you're free to add any other properties as well
   $(".blue-box").animate({
-  	"margin-left": "+=200px" //+= this will just increase the margin you have by 200px
-  }, 1000, "linear");// here over 1000 milliseconds jQuery go ahead and add one pixel every 5 milliseconds in linear scale.
-  //If you don't pass anything as a third parameter by default is usually 'swing'.
+  	"margin-left": "200px",
+  	"opacity": "0",
+  	"height": "50px",
+  	"width": "50px",
+  	"margin-top": "25px"
+  }, 1000);
+
+  $("p").animate({
+  	fontSize: "20px"
+  }, 1000); //This custom animations doesn't work with colors, because jQuery it's not clever enough to transition between the 
+  //color correctly
 });
