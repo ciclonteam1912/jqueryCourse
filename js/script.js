e8e0b4ba08ed679b408b6e64be71898fd9682d06
Juan Moris <moris_juan@hotmail.com>
2017-11-15 10:08:41 -0300

    jQuery Methods for Filtering

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 $(function() {
   // jQuery goes here...
 
-  /*jQuery Methods for Traversal
-	Here we will get any elements form the page using jQuery functions that allow us to traverse the HTML document.
-  */
-  // $("#list").find("li").css("background-color", "rgba(180, 180, 30, 0.8)");
-  // $("#list").parents("div").css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select all the list parents only to the div tag.
-  // $("#list").parent().css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select the direct parent of the list element.
-  // $("#list").children("li").css("background-color", "rgba(180, 180, 30, 0.8)"); // This will select the direct children of the #list element.
+  /*jQuery Methods for Filtering
 
-  // $("#list").siblings().css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select all the siblings elements which are on the same level.
-  //and having the same parent but not the element itself
-  // $("#list").siblings("p").css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select all the p tags that are on the same level
-  // $("#list").siblings(":header").css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select all the headers tags that are on the same level
-  // $("#list").prev().css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select the previous element
-  $("#list").next().css("background-color", "rgba(180, 180, 30, 0.8)");//Here will select the next element
+  */
+  // $("#list").find("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").find("li:even").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("#list").children("li").filter(":even").css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li")filter(function(index){ //The function it's going to get the index of the element you're currently working on
+  // 	return index % 3 === 1;
+  // }).css("background-color", "rgba(180, 180, 30, 0.8)");
+
+  // $("li").first().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li").last().css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li").eq(4).css("background-color", "rgba(180, 180, 30, 0.8)");
+  // $("li").eq(-2).css("background-color", "rgba(180, 180, 30, 0.8)");//This will go back to front
+  // $("li").not(":first").css("background-color", "rgba(180, 180, 30, 0.8)");
+  $("li").not(function(index){
+  	return index % 3 === 2;
+  }).css("background-color", "rgba(180, 180, 30, 0.8)");
+
+
 });
