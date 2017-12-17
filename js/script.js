3e9eef890ebbd0969117e2a8ac5d214d453d6355
Juan Moris <moris_juan@hotmail.com>
2017-12-17 12:05:28 -0300

    Manipulating Attributes and Properties

--- js/script.js
+++ js/script.js
@@ -1,31 +1,39 @@
 $(function() {
   // jQuery goes here...
 
-  /*Removing elements and content */
-  // $("li").remove();
+  /*Manipulating Attributes and Properties*/
+  //attr(), prop(), val() --> for values of inputs elements
 
-  /*Mini-challenge. Remove all the direct children of the form element that are not an input of type text, textarea and br tags*/
-  // $("form").children().not("input:text, textarea, br").remove();
+  /*Tvar specialLink = $("#special-link");
+  console.log(specialLink.attr("href")); his return the value of the href attribute*/
 
-  /*In contrat to the remove funtion it's going to remember all the data and event handlers that are associated with the removed elements.
-  There's a function called detach() which just detaches an element from the page and you can store that element into a variable to be 
-  able then later append it to a DOM element*/
-  // var detachedListItem = $("li").detach();
-  // $("#content").append(detachedListItem);
+ /* specialLink.attr("href", "https://www.udemy.com"); This set a new value for the href attribute*/
 
-  /*You can also do the same thing with remove, but the difference is when you were to re append or re-insert an element into the page,
-  and that element has some kind of click listener or any other kind of event, jQuery it's not going to remember any of that. So all the
-  event handlers and all the associated data are lost.
+ /*There are some cases you don't want to use the attr() function and that it's when there are some boolean properties in HTML5,
+ for example when you add a checkbox to the form, this has a boolean property named checked.
 
-  So if you want to insert a removed element back into the page, you will probably want to use detach() instead because when you detached it
-  jQuery it's not gonna forget all the event handlers that are associated with the element*/
+  So other boolean properties are selected for selected tags and disabled that you can use with prop() function*/
+  var checkbox = $("input:checkbox");
+  /* console.log(checkbox.prop("checked")); This will return the cuurent value of the checkbox, true or false.
+  console.log(checkbox.attr("checked")); This will return checked everytime. And that's why you don't want to use the attr() function*/
+  console.log(checkbox.prop("checked", false));
+  console.log(checkbox.prop("checked")); /*Here will return false*/
+  console.log(checkbox.attr("checked")); /*Here will return checked again*/
 
-  /*With remove() and detach() also remove all the child elements of the selected element*/
+  var textInput = $("input:text");
+  /* console.log(textInput.val()); The val() without parameter you retrieve the value of the input element*/
+  textInput.val("Juan Moris"); /*The val() with a parameter you set the input element to a new value*/
+  console.log(textInput.val()); 
 
-  /*empty() it's going to move everything inside that tag, but it's not going to remove the element itself, it's just going to empty
-  everything inside it.*/
-  // $("p:first").empty();
+  var rangeInput = $("input[type='range']"); /*for range type there's no convenience operator in jQuery*/
+  console.log(rangeInput.val());
 
-  /*Mini-challenge. Empty all the boxes whatever color thay have*/
-  $(".red-box, .green-box, .blue-box").empty();
+  /*Wherever you have multiple elements selected like $("input:text") you may actually select multiple elements so if you have more 
+  than one text input on the page then when you call val(), attr() or prop() on that it's always call implicity call the first. So 
+  it's only going to work with one element at a time. It's not gonna return an array with the values of all the elements or something
+  like that, it's just going to give you the value of the first text input.
+
+  val()  will return the value of only the first selected element when used to read the current value.
+  However, when using val("someValue")  to set the value, it will do this for all the selected elements.
+  So what you're experiencing is the normal behavior. Use first()  to only set the first one, or eq(n)  to only set the n-th one.*/
 });
