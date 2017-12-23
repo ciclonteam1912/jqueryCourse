e660954f19c53ab3ac836521f7ec5be665f23fc8
Juan Moris <moris_juan@hotmail.com>
2017-12-23 19:13:15 -0300

    Changing the CSS Properties of Elements

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 $(function() {
   // jQuery goes here...
 
-  /*Coding Activity: Creating an Image Slideshow!*/
-  var galleryImage = $(".gallery").find("img").first();
-  var images = [
-    "images/laptop-mobile_small.jpg",
-    "images/laptop-on-table_small.jpg",
-    "images/people-office-group-team_small.jpg"
-  ];
+  /*Changing the CSS Properties of Elements*/
+  $(".gallery").css("display", "none"); //hide()
 
-  var i = 0;
-  setInterval(function(){ /*This method is pure javaScript, it's asynchronous*/
-    i = (i + 1) % images.length; /*This will create a sequence of 0,1,2 0,1,2 and so on*/
-    galleryImage.fadeOut(function(){
-      $(this).attr("src", images[i]);
-      $(this).fadeIn();
-    });
-    console.log(galleryImage.attr("src"));
-  }, 5000);
+  /*We can retrieve a value of a property by using only the first parameter*/
+  var redBox = $(".red-box");
+  console.log(redBox.css("width")); //This will return the unit like in this case the pixels.
+  console.log(redBox.width()); //This will return the width (numerical value) withou the unit.
+
+  /*We can set a value property like this*/
+  redBox.css("background-color", "#AA7700");
+  $("p").css("font-size", "18px");
+
+  redBox.css("width", "+=20px"); // This will increase the element by 20 pixels
+
+  var properties = $("p").css(["font-size", "line-height", "color"]); // Select the values of multiple properties
+  console.log(properties); // This will return as an object with a key value pair (Object { font-size: "18px", line-heigth: "21.6px", color: "rgb(0, 0, 0)" })
+  console.log(properties["font-size"]);
+
+  redBox.css("user-select", "none"); //This will make not be selectable the text of the red box
+  redBox.css("user-select", function(){ // You can use a function too
+    return "none";  
+  }); 
+
+  redBox.css("background-color", "#AA7700 !important"); //jQuery will ignore all important declarations, so if you want to use
+  // this you havet use css classes
+
+
 });
