9fb1e9c46cb7b18c6a5f84e0932d49bfd3b9cb53
Juan Moris <moris_juan@hotmail.com>
2017-12-23 21:20:11 -0300

    Retrieving and Changing the Content of an Element

--- js/script.js
+++ js/script.js
@@ -1,25 +1,14 @@
 $(function() {
   // jQuery goes here...
 
-  /*Changing the Data of an Element*/
-  var gallery = $(".gallery");
-
-  var images = [
-    "images/laptop-mobile_small.jpg",
-    "images/laptop-on-table_small.jpg",
-    "images/people-office-group-team_small.jpg"
-  ];
-
-  gallery.data("availableImages", images); // The way to associate data with the element 
-  console.log(gallery.data("availableImages")); // To get the value (it will return the array of images)
-
-  gallery.data("name", "The Awesome Gallery");
-  console.log(gallery.data()); // This will return all the data that it's associated with the gallery object
-
-  gallery.removeData("name");
-  console.log(gallery.data("name")); //This will return undefined because it was already remove
-
+  /*Retrieving and Changing the Content of an Element*/
   var firstPar = $("p:first");
-  console.log(firstPar.data("mydata")); //Here we specify the key that we define on the p tag "data-mydata" that it's called data-attributes
-  //and it will show thw value of that key that is "some data here"
+ // console.log(firstPar.text()); //This will print the actual text of the first paragraph
+ // console.log(firstPar.html()) //Here will print all the html tag and also the text of the paragraph
+
+ // firstPar.text("<strong>Hello</strong> World!"); // This will print the tag strong into the page, this will not apply as an html
+  //firstPar.html("<strong>Hello</strong> World!"); // Here we can make any html code inside the html()
+
+  /*Mini challenge*/
+  firstPar.html(firstPar.html() + " This was just appended.");
 });
