89595fd5570fc024e31c88d6ad71e6c5d6f72663
Juan Moris <moris_juan@hotmail.com>
2017-12-25 17:12:46 -0300

    Adding MouseEnter & MouseLeave Handlers

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(function() {
   // jQuery goes here...
 
-  /*Adding Hover Handlers*/
+  /*Adding MouseEnter & MouseLeave Handlers*/
   $("#btn-hover").hover(function(){ // Here the hover event it's going to get twice because the hover event is going to
     alert("Button was hovered."); // be fired when i enter that button and again when the mouse leaves that element
   });
@@ -9,5 +9,23 @@ $(function() {
   /*Mini challenge: Select the green-box and when that's hovered change the text to "I was hovered!"*/
   $(".green-box").hover(function(){
     $(this).text("I was hovered!");
+  });
+
+  var blueBox = $(".blue-box");
+  /*This has a problem because when you hovered very often the animation queue is going to get all those events and it's going to
+  keep fading the blue-box in and out again. To evoid you can use the stop() function*/
+  // blueBox.mouseenter(function(){
+  //   $(this).stop().fadeTo(500, 0.7);
+  // });
+
+  // blueBox.mouseleave(function(){
+  //   $(this).stop().fadeTo(500, 1);
+  // });
+
+  //hover(handlerIn, handlerOut). This is a shorthand for the mouse events.
+  blueBox.hover(function() {
+     $(this).stop().fadeTo(500, 0.7);
+  }, function(){
+    $(this).stop().fadeTo(500, 1);
   });
 });
