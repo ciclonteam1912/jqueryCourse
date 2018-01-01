0a693fb7e2a2b26dd3fe68f73bd8ae50a6985e77
Juan Moris <moris_juan@hotmail.com>
2018-01-01 18:51:39 -0300

    Coding Activity: Complete Form Validation on Submit

--- js/script.js
+++ js/script.js
@@ -2,28 +2,63 @@ $(function() {
   // jQuery goes here...
 
   /*Handling the Submit Event*/
-  /*The submit() event it's very useful to perform some final validation where you can go through all the elements
-  on the page and see if everything's fine before actually sending that form data to your server.
-
-  So this prevents unnecessary operations on the server, it laso prevents the use of waiting, it prevents the page reload
-  and it just provides a better user experience because the user knows right away what's going wrong.*/
   $("#form").submit(function(event){
-    var textarea = $("#message");
-    if(textarea.val().trim() == ""){
-      textarea.css("box-shadow", "0 0 4px #811");
-      event.preventDefault();
-    } else{
-      //form will be submitted
-    }
+    var name = $("#name").val();
+    var password = $("#password").val();
+    var message = $("#message").val();
+    var checkbox = $("#cb").is(":checked");
+
+    validateNameField(name, event);
+    validatePasswordField(password, event);
+    validateMessageField(message, event);
+    validateCheckboxField(checkbox, event);
   });
-
-  /*Observation:
-
-  The event object is useful to control the event that happened, here the form submission.
-  Without access to the event object, we wouldn't be able to prevent the form from submitting because done 
-  using event.preventDefault(). As the name implies, that function prevents the default action that 
-  normally occurs with the event -- e.g. form submission.
-
-  Another common example is when you handle the click on a link in JS, you can use event.preventDefault()  
-  to stop the link from actually taking the user to the linked page.*/
 }); 
+
+function validateNameField(name, event){
+  if(!isValidName(name)){
+    $("#name-feedback").text("Please enter at least two characters");
+    event.preventDefault();
+  }else{
+    $("#name-feedback").text("");
+  }
+}
+
+function validatePasswordField(password, event){
+  if(!isValidPassword(password)){
+    $("#password-feedback").text("The password should have at least 6 characters and contain a number");
+    event.preventDefault();
+  }else{
+    $("#password-feedback").text("");
+  }
+}
+
+function validateMessageField(message, event){
+  if(!isValidMessage(message)){
+    $("#message-feedback").text("Please enter a message");
+    event.preventDefault();
+  }else{
+    $("#message-feedback").text("");
+  }
+}
+
+function validateCheckboxField(isChecked, event){
+  if(!isChecked){
+    $("#checkbox-feedback").text("Please agree to this");
+    event.preventDefault();
+  }else{
+    $("#checkbox-feedback").text("");
+  }
+}
+
+function isValidName(name){
+  return name.length >= 2;
+}
+
+function isValidPassword(password){
+  return password.length >= 6 && /.*[0-9].*/.test(password);
+}
+
+function isValidMessage(message){
+  return message.trim() != "";
+}
